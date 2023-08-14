5b1617a15c60e8d9c5909fbd7c5639284b79ced9
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-08-14 23:44:15 +0700

    allow cors

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.get("/trash01", async (req, res) => {
         headers: {
             "X-M2m-Origin": "2025004ac7ba5861:6c2b692e59eea2d7",
             "Content-Type": "application/json;ty=3",
-            "Accept": "application/json"
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true"
           },
     })
     const responseJson = await response.json();
@@ -20,15 +22,47 @@ app.get("/trash01", async (req, res) => {
         headers: {
             "X-M2m-Origin": "2025004ac7ba5861:6c2b692e59eea2d7",
             "Content-Type": "application/json;ty=3",
-            "Accept": "application/json"
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true"
           },
     })
     const second_responseJson = await second_response.json();
     const output = second_responseJson['m2m:cin']['con']
     const jsonOutput = JSON.parse(output)
     console.log(typeof(jsonOutput));
-    res.send(jsonOutput.data);
+    res.send(jsonOutput);
+})
 
+app.get("/trash02", async (req, res) => {
+    const response = await fetch(`${BASE_URL}/antares-cse/antares-id/TrashIot/Trash-02?fu=1&ty=4&drt=1`, {
+        method: "GET",
+        headers: {
+            "X-M2m-Origin": "2025004ac7ba5861:6c2b692e59eea2d7",
+            "Content-Type": "application/json;ty=3",
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true"
+          },
+    })
+    const responseJson = await response.json();
+    const newestData = responseJson['m2m:uril'][0]
+    
+    const second_response = await fetch(`${BASE_URL}${newestData}`, {
+        method: "GET",
+        headers: {
+            "X-M2m-Origin": "2025004ac7ba5861:6c2b692e59eea2d7",
+            "Content-Type": "application/json;ty=3",
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true"    
+          },
+    })
+    const second_responseJson = await second_response.json();
+    const output = second_responseJson['m2m:cin']['con']
+    const jsonOutput = JSON.parse(output)
+    console.log(typeof(jsonOutput));
+    res.send(jsonOutput);
 })
 
 app.listen(3000, () => {
